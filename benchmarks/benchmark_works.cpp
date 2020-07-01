f1249f25808ba10bc057718c9ce5463024284cfb
TmLev <leva.bugatti@gmail.com>
2020-07-01 19:44:04 +0300

    feat: add dummy benchmark

---
+++ benchmarks/benchmark_works.cpp
@@ -0,0 +1,23 @@
+#include <mutex>
+#include <memory>
+
+#include <benchmark/benchmark.h>
+
+int counter = 0;
+std::shared_ptr<std::mutex> mutex;
+
+static auto Run(benchmark::State& state) -> void {
+  if (state.thread_index == 0) {
+    counter = 0;
+    mutex = std::make_shared<std::mutex>();
+  }
+
+  while (state.KeepRunning()) {
+    std::unique_lock lock(*mutex);
+    ++counter;
+  }
+}
+
+BENCHMARK(Run)->UseRealTime()->Threads(1)->Threads(2)->Threads(4)->Threads(8);
+
+BENCHMARK_MAIN();
