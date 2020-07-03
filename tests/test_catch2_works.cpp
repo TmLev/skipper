033b952a6a8411a7a5badeaa9bcc392c560304bb
TmLev <leva.bugatti@gmail.com>
2020-07-03 14:42:48 +0300

    tests: remove failing test

--- tests/test_catch2_works.cpp
+++ tests/test_catch2_works.cpp
@@ -1,12 +1,7 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
-TEST_CASE("Catch2 works / correct test") {
+TEST_CASE("Catch2 works / stub test") {
   constexpr size_t kOne = 1;
   REQUIRE(kOne + 1 == 2);
-}
-
-TEST_CASE("Catch2 works / invalid test") {
-  constexpr size_t kOne = 1;
-  REQUIRE(kOne + 1 != 2);
 }
