6967ef8b620358572a721701a1b89907ee9a1947
TmLev <leva.bugatti@gmail.com>
2020-07-03 15:35:44 +0300

    style: apply clang-format

--- skipper/sequential/sequential.hpp
+++ skipper/sequential/sequential.hpp
@@ -1,7 +1,8 @@
 #include <vector>
 
 template <class T>
-auto Print(const std::vector<T>& vector, const std::string_view& delimiter) -> void;
+auto Print(const std::vector<T>& vector, const std::string_view& delimiter)
+    -> void;
 
 // TODO: fix CMake to specify full path (skipper/sequential/sequential.ipp)
 #include "sequential.ipp"
