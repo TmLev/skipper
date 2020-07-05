0f8cf6b6a0ee80e447f8ebf2fab6204abe4447e9
TmLev <leva.bugatti@gmail.com>
2020-07-05 23:11:16 +0300

    skipper: update main

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 
-#include "skipper/sequential/sequential.hpp"
+#include "skipper/sequential.hpp"
 
 int main() {
   auto countdown = std::vector{1, 2, 3};
-  Print(
-      countdown,
-      ", "
-);
+  skipper::Print(countdown, ", ");
   std::cout << "! Skipper, we are ready!" << std::endl;
   return 0;
 }
