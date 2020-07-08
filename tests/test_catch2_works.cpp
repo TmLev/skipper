465e763dcee53e485fce761509a38cb0d976a66f
TmLev <leva.bugatti@gmail.com>
2020-07-08 15:22:11 +0300

    tests: rename stub catch2 test

--- tests/test_catch2_works.cpp
+++ tests/test_catch2_works.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
-TEST_CASE("Catch2 works / stub test") {
+TEST_CASE("Catch2 works") {
   constexpr size_t kOne = 1;
   REQUIRE(kOne + 1 == 2);
 }
