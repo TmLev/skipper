1693c2f6c5286c732c5f2f8efe860ec909e41976
TmLev <leva.bugatti@gmail.com>
2020-07-08 15:31:48 +0300

    ci: fix directories for run-clang-format

--- .ci-scripts/clang-format.sh
+++ .ci-scripts/clang-format.sh
@@ -6,4 +6,4 @@ python3 third-party/run-clang-format/run-clang-format.py \
   --clang-format-executable clang-format-10 \
   --extensions hpp,ipp,cpp \
   --recursive \
-  skipper tests benchmarks
+  include/skipper tests benchmarks
