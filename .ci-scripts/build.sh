0300c0a84c286d7d94f4f4b5f26d7ec9996039b3
TmLev <leva.bugatti@gmail.com>
2020-07-14 13:11:45 +0300

    Revert "ci: modernize build script"
    This reverts commit d80bbb708d5ec8cf3b75e1b2f489d95047cf5154.

--- .ci-scripts/build.sh
+++ .ci-scripts/build.sh
@@ -3,8 +3,9 @@
 # Assume that script is running in the root directory.
 
 build() {
-  cmake -S . -B build
-  cmake --build build
+  mkdir build && cd build
+  cmake ..
+  cmake --build . -- -j2
 }
 
 (build)
