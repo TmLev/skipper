8d280841013c0e25a6834d9e4b26bb19fdbed3c6
TmLev <leva.bugatti@gmail.com>
2020-07-14 21:32:26 +0300

    cmake: add clang-tidy analyzer

---
+++ cmake/StaticAnalyzers.cmake
@@ -0,0 +1,14 @@
+option(ENABLE_CLANG_TIDY "Enable static analysis with clang-tidy" ON)
+if (NOT ENABLE_CLANG_TIDY)
+  message(STATUS "clang-tidy is disabled")
+  return()
+endif ()
+
+find_program(CLANG_TIDY_FOUND "clang-tidy-10")
+if (NOT CLANG_TIDY_FOUND)
+  message(SEND_ERROR "clang-tidy was not found")
+  return()
+endif ()
+
+set(CMAKE_CXX_CLANG_TIDY "clang-tidy-10" -extra-arg=-Wno-unknown-warning-option)
+message(STATUS "clang-tidy is found and enabled")
