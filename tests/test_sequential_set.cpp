773a379ec900ec3e33c65545682d18c28cbfbd54
TmLev <leva.bugatti@gmail.com>
2020-08-22 15:51:17 +0300

    tests: add more

--- tests/test_sequential_set.cpp
+++ tests/test_sequential_set.cpp
@@ -1,26 +1,47 @@
 #include <catch2/catch.hpp>
 
+#include <set>
 #include <sstream>
 #include <utility>
 #include <vector>
+
+#include "../utils/random.hpp"
 
 #include "skipper/sequential_set.hpp"
 
 template <typename T>
 using SL = skipper::SequentialSkipListSet<T>;
 
-TEST_CASE("Empty SL has same Begin() and End() iterators") {
+TEST_CASE("Find() returns End() iterator when no element was found", "[Find]") {
   auto skip_list = SL<int>{};
-  REQUIRE(skip_list.Begin() == skip_list.End());
+
+  {
+    auto it = skip_list.Find(2);
+    REQUIRE(it == skip_list.End());
+  }
+  {
+    skip_list.Insert(3);
+    auto it = skip_list.Find(2);
+    REQUIRE(it == skip_list.End());
+  }
 }
 
-TEST_CASE("Find() in empty SL returns End() iterator") {
+TEST_CASE("Find() returns equal iterators for same number", "[Find]") {
   auto skip_list = SL<int>{};
+
+  skip_list.Insert(2);
+
   auto it = skip_list.Find(2);
-  REQUIRE(it == skip_list.End());
+  auto same_it = skip_list.Find(2);
+  REQUIRE(it == skip_list.Begin());
+  REQUIRE(it == same_it);
+  REQUIRE(*it == 2);
+
+  auto other = skip_list.Find(3);
+  REQUIRE(it != other);
 }
 
-TEST_CASE("Insert()-ing same element returns same iterator") {
+TEST_CASE("Insert() returns same iterator for same element", "[Insert]") {
   auto skip_list = SL<int>{};
 
   auto [it, success] = skip_list.Insert(1);
@@ -34,31 +55,31 @@ TEST_CASE("Insert()-ing same element returns same iterator") {
 
   auto [other_it, other_success] = skip_list.Insert(0);
   REQUIRE(other_success);
-  REQUIRE(other_it == skip_list.Begin());
   REQUIRE(it != other_it);
 }
 
-TEST_CASE("Insert() maintains sortedness") {
+TEST_CASE("Insert() maintains sortedness", "[Insert]") {
   auto skip_list = SL<int>{};
 
-  skip_list.Insert(2);
-  skip_list.Insert(1);
-  skip_list.Insert(0);
-
-  auto got = std::vector<int>{};
-  for (auto it = skip_list.Begin(); it != skip_list.End(); ++it) {
-    got.push_back(*it);
+  auto numbers = GenerateNumbers(100'000, -10'000, 10'000);
+  for (auto n : numbers) {
+    skip_list.Insert(n);
   }
-  auto expected = std::vector<int>{0, 1, 2};
-  REQUIRE(got == expected);
+
+  auto sorted_numbers = std::set<int>{std::begin(numbers), std::end(numbers)};
+  auto it = skip_list.Begin();
+  for (auto n : sorted_numbers) {
+    REQUIRE(*it == n);
+    ++it;
+  }
 }
 
-TEST_CASE("Erase() does nothing if SL is empty") {
+TEST_CASE("Erase() does nothing if SL is empty", "[Erase]") {
   auto skip_list = SL<int>{};
   REQUIRE(skip_list.Erase(0) == 0);
 }
 
-TEST_CASE("Erase() erases same element only once") {
+TEST_CASE("Erase() erases same element only once", "[Erase]") {
   auto skip_list = SL<int>{};
   skip_list.Insert(1);
   REQUIRE(skip_list.Erase(1) == 1);
@@ -66,12 +87,61 @@ TEST_CASE("Erase() erases same element only once") {
   REQUIRE(skip_list.Erase(1) == 0);
 }
 
-TEST_CASE("Iterator::operator->() returns pointer to value") {
+TEST_CASE("Erase() empties SL after removing all elements", "[Erase]") {
+  auto skip_list = SL<int>{};
+
+  auto numbers = GenerateNumbers(100'000, -10'000, 10'000);
+  numbers.push_back(numbers.front() - 1);  // In case all numbers are the same
+  for (auto n : numbers) {
+    skip_list.Insert(n);
+  }
+  REQUIRE(skip_list.Begin() != skip_list.End());
+
+  for (auto n : numbers) {
+    skip_list.Erase(n);
+  }
+  REQUIRE(skip_list.Begin() == skip_list.End());
+}
+
+TEST_CASE("Iterator: empty SL has same Begin() and End()", "[Iterator]") {
+  auto skip_list = SL<int>{};
+  REQUIRE(skip_list.Begin() == skip_list.End());
+}
+
+TEST_CASE("Iterator: operator->() returns pointer to value", "[Iterator]") {
   auto skip_list = SL<std::string>{};
   auto value = std::string{"123"};
   skip_list.Insert(value);
   auto it = skip_list.Find(value);
   REQUIRE(it->size() == value.size());
+}
+
+TEST_CASE("Iterator: properly increments", "[Iterator]") {
+  auto skip_list = SL<int>{};
+
+  skip_list.Insert(1);
+  skip_list.Insert(2);
+  skip_list.Insert(3);
+
+  auto one = skip_list.Find(1);
+  auto two = skip_list.Find(2);
+  auto three = skip_list.Find(3);
+
+  REQUIRE(one != two);
+  REQUIRE(two != three);
+  REQUIRE(three != one);
+
+  auto one_copy = one++;
+  REQUIRE(one == two);
+  REQUIRE(one != one_copy);
+
+  auto two_copy = ++two;
+  REQUIRE(two == two_copy);
+  REQUIRE(two == three);
+
+  auto three_copy = three++;
+  REQUIRE(two == three_copy);
+  REQUIRE(three == skip_list.End());
 }
 
 struct MovableOnly {
