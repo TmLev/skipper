9a18c4cf2c95c26506195713e3ca7622e341f266
TmLev <leva.bugatti@gmail.com>
2020-08-25 14:37:49 +0300

    scripts: fix cd when directory was not found

--- scripts/benchmarks.sh
+++ scripts/benchmarks.sh
@@ -3,7 +3,7 @@
 # Assume that project was built in directory `ROOT_DIR/build`.
 
 run_benchmarks() {
-  cd build/benchmarks || echo "Benchmarks directory not found" && exit 1
+  cd build/benchmarks || { echo "Benchmarks directory not found"; exit 11; }
   benchmark_names=$(find . -maxdepth 1 -name "benchmark_*")
 
   for benchmark in "${benchmark_names[@]}"; do
--- scripts/tests.sh
+++ scripts/tests.sh
@@ -3,8 +3,8 @@
 # Assume that project was built in directory `ROOT_DIR/build`.
 
 run_tests() {
-  cd build/tests || echo "Tests directory not found" && exit 1
-  test_names=$(find . -maxdepth 1 -name "test_*")
+  cd build/tests || { echo "Tests directory not found"; exit 11; }
+  test_names=$(find . -maxdepth 1 -name "test_*" | sed "s|^\./||")
 
   for test in "${test_names[@]}"; do
     echo "Running \"$test\"..."
