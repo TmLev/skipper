4c8b9ec3820fcaf69d5704935a8a6add7c8b350f
TmLev <leva.bugatti@gmail.com>
2020-08-25 14:16:56 +0300

    scripts: find files with tests, benchmarks automatically. resolve #13

--- scripts/benchmarks.sh
+++ scripts/benchmarks.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Assume that project was built in directory `ROOT_DIR/build`.
 
-benchmark_names=(
-  "benchmark_sequential_set"
-)
-
 run_benchmarks() {
-  cd build/benchmarks
+  cd build/benchmarks || echo "Benchmarks directory not found" && exit 1
+  benchmark_names=$(find . -maxdepth 1 -name "benchmark_*")
+
   for benchmark in "${benchmark_names[@]}"; do
     chmod +x "$benchmark" && ./"$benchmark"
   done
--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Assume that project was built in directory `ROOT_DIR/build`.
 
-test_names=(
-  "test_sequential_set"
-)
-
 run_tests() {
-  cd build/tests
+  cd build/tests || echo "Tests directory not found" && exit 1
+  test_names=$(find . -maxdepth 1 -name "test_*")
+
   for test in "${test_names[@]}"; do
     echo "Running \"$test\"..."
     chmod +x "$test" && ./"$test"
