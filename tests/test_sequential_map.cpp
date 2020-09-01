30de1ab52de982b6ecbae12cd4d319fdf0de3d34
Artem Belyakov <belyakov@smartdec.ru>
2020-09-01 17:39:41 +0300

    tests: add some cases for coverage

--- tests/test_sequential_map.cpp
+++ tests/test_sequential_map.cpp
@@ -8,3 +8,58 @@
 #include "../utils/random.hpp"
 
 #include "skipper/sequential_map.hpp"
+
+template <typename Key, typename Value>
+using SM = skipper::SequentialSkipListMap<Key, Value>;
+
+TEST_CASE("Check Sequential SkipList Map coverage", "[Coverage]") {
+  auto skip_list = SM<int, int>{};
+  auto [it1, success1] = skip_list.Insert(1, 1);
+  auto [it2, success2] = skip_list.Insert(2, 2);
+
+  SECTION("Iterator::operator*") {
+    REQUIRE((*it1).first == 1);
+  }
+  SECTION("Iterator::operator->") {
+    REQUIRE(it1->first == 1);
+  }
+  SECTION("Iterator::operator++") {
+    auto copy = it1;
+    ++copy;
+    REQUIRE(copy->first == it2->first);
+  }
+  SECTION("Iterator::operator++(int)") {
+    auto copy = it1;
+    copy++;
+    REQUIRE(copy->first == it2->first);
+  }
+  SECTION("Iterator::operator==") {
+    auto copy = it1;
+    ++copy;
+    REQUIRE(copy == it2);
+  }
+  SECTION("Iterator::operator!=") {
+    REQUIRE(it1 != it2);
+  }
+
+  SECTION("Find()") {
+    auto f1 = skip_list.Find(1);
+    REQUIRE(f1 == it1);
+
+    auto f3 = skip_list.Find(3);
+    REQUIRE(f3 != it1);
+  }
+  SECTION("Insert()") {
+    auto [it, success] = skip_list.Insert(1, 1);
+    REQUIRE(!success);
+
+    auto [it5, success5] = skip_list.Insert(5, 5);
+    REQUIRE(success5);
+    REQUIRE(it5->first == 5);
+    REQUIRE(it5->second == 5);
+  }
+  SECTION("operator[]") {
+    int value = skip_list[1];
+    REQUIRE(value == it1->second);
+  }
+}
