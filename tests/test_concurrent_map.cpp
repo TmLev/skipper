7be7529a74c02c2581c0a8805a14e882b105f076
Artem Belyakov <belyakov@smartdec.ru>
2020-09-20 11:05:01 +0300

    feat: add more concurrent tests

--- tests/test_concurrent_map.cpp
+++ tests/test_concurrent_map.cpp
@@ -110,6 +110,38 @@ TEST_CASE("Two threads insert repeating numbers simultaneously",
   }
 }
 
+TEST_CASE("Two threads insert the same numbers simultaneously",
+          "[Concurrency]") {
+  auto skip_list = SL<int, int>{};
+
+  auto first_numbers = chunk(10 * kThousand, random(0, kThousand)).get();
+  auto second_numbers =
+      std::vector<int>(std::begin(first_numbers), std::end(first_numbers));
+
+  auto first = std::thread([&]() {
+    for (auto n : first_numbers) {
+      skip_list.Insert(n, n);
+    }
+  });
+
+  auto second = std::thread([&]() {
+    for (auto n : second_numbers) {
+      skip_list.Insert(n, n);
+    }
+  });
+
+  first.join();
+  second.join();
+
+  auto merged = std::vector<int>{};
+  std::merge(std::begin(first_numbers), std::end(first_numbers),
+             std::begin(second_numbers), std::end(second_numbers),
+             std::back_inserter(merged));
+  for (auto n : merged) {
+    REQUIRE(skip_list.Contains(n));
+  }
+}
+
 TEST_CASE("Two threads insert different repeating numbers simultaneously",
           "[Concurrency]") {
   auto skip_list = SL<int, int>{};
@@ -203,4 +235,99 @@ TEST_CASE("One thread inserts and second tries to erase the same element",
   } else {
     REQUIRE(skip_list.Erase(num));
   }
+}
+
+TEST_CASE("One thread inserts and second check for contain the same element",
+          "[Concurrency]") {
+  auto skip_list = SL<int, int>{};
+
+  int num = 10;
+
+  auto first_numbers = chunk(kThousand, random(num, num)).get();
+  auto second_numbers = chunk(kThousand, random(num, num)).get();
+
+  auto first = std::thread([&]() {
+    for (auto n : first_numbers) {
+      skip_list.Insert(n, n);
+    }
+  });
+
+  auto second = std::thread([&]() {
+    for (auto n : second_numbers) {
+      skip_list.Contains(n);
+    }
+  });
+
+  first.join();
+  second.join();
+
+  REQUIRE(skip_list.Contains(num));
+}
+
+TEST_CASE(
+    "Tho threads insert different numbers, then four threads check for contain",
+    "[Concurrency]") {
+  auto skip_list = SL<int, int>{};
+
+  auto first_write_numbers =
+      chunk(10 * kThousand, random(0, kThousand - 1)).get();
+  auto second_write_numbers =
+      chunk(10 * kThousand, random(kThousand, 2 * kThousand)).get();
+
+  auto first_write_size = static_cast<int>(first_write_numbers.size());
+  auto second_write_size = static_cast<int>(second_write_numbers.size());
+
+  auto first_read_numbers =
+      std::vector<int>(std::begin(first_write_numbers),
+                       std::begin(first_write_numbers) + first_write_size / 2);
+  auto second_read_numbers = std::vector<int>(
+      std::begin(first_write_numbers) + first_write_size / 2 + 1,
+      std::end(first_write_numbers));
+
+  auto third_read_numbers = std::vector<int>(
+      std::begin(second_write_numbers),
+      std::begin(second_write_numbers) + second_write_size / 2);
+  auto fourth_read_numbers = std::vector<int>(
+      std::begin(second_write_numbers) + second_write_size / 2 + 1,
+      std::end(second_write_numbers));
+
+  auto first_write = std::thread([&]() {
+    for (auto n : first_write_numbers) {
+      skip_list.Insert(n, n);
+    }
+  });
+  auto second_write = std::thread([&]() {
+    for (auto n : second_write_numbers) {
+      skip_list.Insert(n, n);
+    }
+  });
+
+  first_write.join();
+  second_write.join();
+
+  auto first_read = std::thread([&]() {
+    for (auto n : first_read_numbers) {
+      REQUIRE(skip_list.Contains(n));
+    }
+  });
+  auto second_read = std::thread([&]() {
+    for (auto n : second_read_numbers) {
+      REQUIRE(skip_list.Contains(n));
+    }
+  });
+  auto third_read = std::thread([&]() {
+    for (auto n : third_read_numbers) {
+      REQUIRE(skip_list.Contains(n));
+    }
+  });
+  auto fourth_read = std::thread([&]() {
+    for (auto n : fourth_read_numbers) {
+      REQUIRE(skip_list.Contains(n));
+    }
+  });
+
+  first_read.join();
+  second_read.join();
+  third_read.join();
+  fourth_read.join();
 }
