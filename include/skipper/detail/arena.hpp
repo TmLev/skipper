654cc1837370b2faae20b1ac6497821ee44e0b9f
TmLev <leva.bugatti@gmail.com>
2020-09-24 13:21:21 +0300

    feat: add arena allocator

---
+++ include/skipper/detail/arena.hpp
@@ -0,0 +1,42 @@
+#ifndef SKIPPER_DETAIL_ARENA_HPP
+#define SKIPPER_DETAIL_ARENA_HPP
+
+#include <atomic>
+#include <memory>
+
+#include "skipper/detail/allocator.hpp"
+
+namespace skipper::detail {
+
+class Arena : public Allocator {
+ private:
+  struct Cell;
+
+ public:
+  static constexpr auto kMaxSize = 10'000'000;
+
+ public:
+  Arena() = default;
+
+  // Copying is not allowed
+  Arena(const Arena& other) = delete;
+  Arena& operator=(const Arena& other) = delete;
+
+  // Allocator interface
+  auto Allocate(std::size_t bytes) -> char* override;
+
+ private:
+  using Memory = std::vector<Cell>;
+  using Cursor = std::atomic<std::size_t>;
+
+ private:
+  Memory memory_{kMaxSize};
+  Cursor cursor_{0};
+};
+
+
+}  // namespace skipper::detail
+
+#endif  // SKIPPER_DETAIL_ARENA_HPP
+
+#include "skipper/detail/arena.ipp"
---
+++ include/skipper/detail/arena.ipp
@@ -0,0 +1,44 @@
+#ifndef SKIPPER_DETAIL_ARENA_IPP
+#define SKIPPER_DETAIL_ARENA_IPP
+
+#include "skipper/detail/arena.hpp"
+
+namespace skipper::detail {
+
+////////////////////////////////////////////////////////////////////////////////
+////
+//// Arena::Cell
+////
+
+struct Arena::Cell {
+ public:
+  ~Cell();
+
+ public:
+  char* raw{nullptr};
+};
+
+Arena::Cell::~Cell() {
+  delete[] raw;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////
+//// Arena: public interface
+////
+
+auto Arena::Allocate(std::size_t bytes) -> char* {
+  auto c = cursor_.fetch_add(1);
+  if (c >= kMaxSize) {
+    return nullptr;
+  }
+
+  auto& cell = memory_[c];
+  cell.raw = new char[bytes];
+
+  return cell.raw;
+}
+
+}  // namespace skipper::detail
+
+#endif  // SKIPPER_DETAIL_ARENA_IPP
