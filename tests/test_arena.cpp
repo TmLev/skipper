4b8586b938ddf4f88f001e9ad6ad4788bd9684e1
TmLev <leva.bugatti@gmail.com>
2020-09-24 13:21:31 +0300

    feat: add simple test for arena

---
+++ tests/test_arena.cpp
@@ -0,0 +1,20 @@
+#include <catch2/catch.hpp>
+
+#include <iostream>
+#include <sstream>
+
+#include "skipper/detail/arena.hpp"
+
+TEST_CASE("Arena allocates", "[Correctness]") {
+  auto arena = skipper::detail::Arena{};
+
+  auto ss = std::stringstream{};
+
+  for (int i = 0; i < 4; ++i) {
+    auto raw = arena.Allocate(sizeof(int));
+    auto number = new(raw) int{i * i};
+    ss << *number << ' ';
+  }
+
+  REQUIRE(ss.str() == "0 1 4 9 ");
+}
