14818c4258f28ba8e2d3547fe89c2ce0806eb377
TmLev <leva.bugatti@gmail.com>
2020-09-24 13:21:08 +0300

    feat: add allocator interface

---
+++ include/skipper/detail/allocator.hpp
@@ -0,0 +1,16 @@
+#ifndef SKIPPER_DETAIL_ALLOCATOR_HPP
+#define SKIPPER_DETAIL_ALLOCATOR_HPP
+
+#include <cstddef>  // std::size_t
+
+namespace skipper::detail {
+
+class Allocator {
+ public:
+  virtual auto Allocate(std::size_t bytes) -> char* = 0;
+  virtual ~Allocator() = default;
+};
+
+}  // namespace skipper::detail
+
+#endif  // SKIPPER_DETAIL_ALLOCATOR_HPP
